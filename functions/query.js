9d2293d3d97fad779eb678253f52cfc882b1b29c
Liz Lam <lizlam.217@gmail.com>
2020-06-17 23:43:41 -0700

    Initial add of function.

---
+++ functions/query.js
@@ -0,0 +1,19 @@
+import faunadb from 'faunadb'
+
+const q = faunadb.query
+const client = new faunadb.Client({ secret: 'fnADuk94M0AGDH2jMwUywRTfJ7LaHkp8lbzxnzPG' })
+
+exports.handler = (event, context, callback) => {
+  const data = JSON.parse(event.body)
+  return client.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection('orders'))),
+      q.Lambda("x", q.Get(q.Var("x")))
+    ))
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
